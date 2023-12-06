ae60c337085c5fcb13ed84bd7d2e3959290a66a0
Beth Hampshire <Beth.Hampshire@mariecurie.org.uk>
2023-12-06 17:13:43 +0000

    Remove unused screen

--- frontend/web/tests/login.test.tsx
+++ frontend/web/tests/login.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from '../components/login';
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 describe("Login test cases", () => {
     it("Render check", () => {
