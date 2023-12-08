ab9b46b942624df77867f35858ccef1f2ad056d5
Beth Hampshire <Beth.Hampshire@mariecurie.org.uk>
2023-12-08 14:15:13 +0000

    Add tests to login

--- frontend/web/services/userSlice.test.ts
+++ frontend/web/services/userSlice.test.ts
@@ -1,48 +1,107 @@
-import { reducer, actions, initialState} from "./userSlice";
+import { reducer, actions, initialState, login } from "./userSlice";
 import { mockUser } from "../mocks/user";
 
 const updatedState = {
-    jwt: mockUser.jwt,
-    username: mockUser.user.username,
-    email: mockUser.user.email
+  jwt: mockUser.jwt,
+  username: mockUser.user.username,
+  email: mockUser.user.email,
 };
 
+const loginData = {
+  identifier: mockUser.user.email,
+  password: mockUser.user.password,
+};
+
+const requestId: string = "someoid";
+
 describe("User slice check", () => {
-    describe("Update state actions",  () => {
-        it("should update the full state", () => {
-            expect(reducer(initialState, actions.update(updatedState))).toEqual(
-                updatedState
-            );
-        });
+  describe("Update state actions", () => {
+    it("should update the full state", () => {
+      expect(reducer(initialState, actions.update(updatedState))).toEqual(
+        updatedState
+      );
+    });
 
-        it("should only update the jwt", () => {
-            expect(
-                reducer(
-                    initialState, 
-                    actions.update({
-                        jwt: updatedState.jwt,
-                    })
-                )
-            ).toEqual({
-                ...initialState,
-                jwt: updatedState.jwt,
-            });
-        });
+    it("should only update the jwt", () => {
+      expect(
+        reducer(
+          initialState,
+          actions.update({
+            jwt: updatedState.jwt,
+          })
+        )
+      ).toEqual({
+        ...initialState,
+        jwt: updatedState.jwt,
+      });
+    });
 
-        it("should clear the state", () => {
-            const stateWithUpdatedValues = reducer(
+    it("should clear the state", () => {
+      const stateWithUpdatedValues = reducer(
+        initialState,
+        actions.update(updatedState)
+      );
+
+      expect(stateWithUpdatedValues).toEqual(updatedState);
+
+      expect(reducer(stateWithUpdatedValues, actions.clear())).toEqual({
+        jwt: "",
+        username: "",
+        email: "",
+      });
+    });
+  });
+
+  describe("Login state flow", () => {
+    it("should set the request state to pending", () => {
+      expect(
+        reducer(initialState, login.pending(requestId, loginData))
+      ).toEqual({
+        ...initialState,
+        requestState: "pending",
+      });
+    });
+
+    it("should set the request state to fulfilled and reset any previous errors", () => {
+      expect(
+        reducer(
+          {
+            ...initialState,
+            error: {
+              message: "Rejected",
+            },
+          },
+          login.fulfilled(
+            {
+              jwt: updatedState.jwt,
+              user: {
+                username: updatedState.username,
+                email: updatedState.email,
+              },
+            },
+            requestId,
+            loginData
+          )
+        )
+      ).toEqual({
+        ...updatedState,
+        requestState: "fulfilled",
+      });
+    });
+
+    it("should set the request state to rejected", () => {
+        const payloadError = { error: {name: "500", message: "Server error" } };
+
+        expect(
+            reducer(
                 initialState,
-                actions.update(updatedState)
-            );
-
-            expect(stateWithUpdatedValues).toEqual(updatedState);
-
-            expect(reducer(stateWithUpdatedValues, actions.clear())).toEqual({
-                jwt: "",
-                username: "",
-                email: "",
-            });
+                login.rejected({} as Error, requestId, loginData, payloadError)
+            )
+        ).toEqual({
+            ...initialState,
+            error: payloadError.error,
+            requestState: "rejected"
         });
     });
+  });
 });
-
