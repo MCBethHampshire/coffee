32485b6e94deddcc5f50608adc30dd72b8b95d0f
Beth Hampshire <Beth.Hampshire@mariecurie.org.uk>
2023-12-08 15:08:34 +0000

    Setup server

--- frontend/web/jest.setup.js
+++ frontend/web/jest.setup.js
@@ -1,1 +1,24 @@
 import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+import "whatwg-fetch";
+
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
